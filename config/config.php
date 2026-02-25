600b904781bea0b99ddbcca3a07bf80d9a9562bd
Kaushal Rangani <codecrusher0@gmail.com>
2026-02-25 14:41:28 +0530

    Update API_KEY placeholder in config.

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@ ini_set('log_errors', 1);
 ini_set('error_log', __DIR__ . '/../logs/debug.log');
 error_reporting(E_ALL);
 
-define('API_KEY', 'AIzaSyAJgHYnjxUX-V1bmJBosHSqnRPdjbuv-PU');
+define('API_KEY', 'Your AIP Key Here');
 define('API_URL', 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-thinking-exp-01-21:generateContent');
 
 
